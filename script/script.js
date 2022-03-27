40564b5bde4affc0df31e9b568dae05ddee7e05e
Kushal Vadodaria <kushalv238@gmail.com>
2022-03-27 11:41:25 +0530

    fixed calculation issue
    JS:
       1. seat number var was converted to integer(initially string, which caused the issue of concatenating seatNo var instead of adding).
       2. Instead of console.logging the result in function berthType, it now returns berth type and is console.logged in function getSeatNo.
    
    HTML:
       1. Removed useless commented web linked files(font awesome, google fonts & bootstrap).

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
 function getSeatNo() {
-    var seatNo = document.getElementById("sn").value;
-    berthType(seatNo)
+    var seatNo = Number(document.getElementById("sn").value);
+    console.log(berthType(seatNo));
 }
 
 function berthType(sn) {
     if(((sn-1) % 4 == 0 && ((sn-1)/4) % 2 == 0) || sn % 4 == 0 && (sn/4) % 2 != 0) {
-        console.log("Seat number " + sn + " is Lower Berth");
+        return ("Seat number " + sn + " is Lower Berth");
     }
     
     else if(((sn-2) % 4 == 0 && ((sn-2)/4) % 2 == 0) || ((sn-1) % 4 == 0 && ((sn-1)/4) % 2 != 0)) {
-        console.log("Seat number " + sn + " is Middle Berth");
+        return ("Seat number " + sn + " is Middle Berth");
     }
     
     else if(((sn+1) % 4 == 0 && ((sn+1)/4) % 2 != 0) || ((sn-2) % 4 == 0 && ((sn-2)/4) % 2 != 0)) {
-        console.log("Seat number " + sn + " is Upper Berth");
+        return ("Seat number " + sn + " is Upper Berth");
     }
     
     else if((sn+1) % 4 == 0 && ((sn+1)/4) % 2 == 0) {
-        console.log("Seat number " + sn + " is Side Lower Berth");
+        return ("Seat number " + sn + " is Side Lower Berth");
     }
     
     else if((sn) % 4 == 0 && ((sn)/4) % 2 == 0) {
-        console.log("Seat number " + sn + " is Side Upper Berth");
+        return ("Seat number " + sn + " is Side Upper Berth");
     }
 
     else {
-        console.log("Seat number " + sn + " is Incorrect!")
+        return ("Seat number " + sn + " is Incorrect!");
     }
 }
