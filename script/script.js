4731057834605a931fde46117b746a13d74e8126
Kushal Vadodaria <kushalv238@gmail.com>
2022-03-27 14:25:13 +0530

    improved code efficiency
    JS:
       1. Used parseInt() instead of Number() to convert var seatNo to int(from string).
       3. Error handling conditions added in line 2.
       2. In berthType function, condition to check whether var sn is divisible by 4 or not, removed. (Unlike c, c++ when indivisible number are divided the result is not rounded down)

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
 function getSeatNo() {
-    var seatNo = Number(document.getElementById("sn").value);
+    var seatNo = parseInt((document.getElementById("sn") || {}).value || "0");
     console.log(berthType(seatNo));
 }
 
 function berthType(sn) {
-    if(((sn-1) % 4 == 0 && ((sn-1)/4) % 2 == 0) || sn % 4 == 0 && (sn/4) % 2 != 0) {
+    if((((sn-1)/4) % 2 == 0) || (sn/4) % 2 == 1) {
         return ("Seat number " + sn + " is Lower Berth");
     }
     
-    else if(((sn-2) % 4 == 0 && ((sn-2)/4) % 2 == 0) || ((sn-1) % 4 == 0 && ((sn-1)/4) % 2 != 0)) {
+    else if((((sn-2)/4) % 2 == 0) || (((sn-1)/4) % 2 == 1)) {
         return ("Seat number " + sn + " is Middle Berth");
     }
     
-    else if(((sn+1) % 4 == 0 && ((sn+1)/4) % 2 != 0) || ((sn-2) % 4 == 0 && ((sn-2)/4) % 2 != 0)) {
+    else if((((sn+1)/4) % 2 == 1) || (((sn-2)/4) % 2 == 1)) {
         return ("Seat number " + sn + " is Upper Berth");
     }
     
-    else if((sn+1) % 4 == 0 && ((sn+1)/4) % 2 == 0) {
+    else if(((sn+1)/4) % 2 == 0) {
         return ("Seat number " + sn + " is Side Lower Berth");
     }
     
-    else if((sn) % 4 == 0 && ((sn)/4) % 2 == 0) {
+    else if(((sn)/4) % 2 == 0) {
         return ("Seat number " + sn + " is Side Upper Berth");
     }
 
